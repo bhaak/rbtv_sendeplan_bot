425476908bf57368e29dff5c229abbc35de0209a
Patric Mueller <bhaak@gmx.net>
2020-07-27 20:04:55 +0200

    New feature: reddit posting

--- lib/rbtv_sendeplan_bot.rb
+++ lib/rbtv_sendeplan_bot.rb
@@ -1,25 +1,59 @@
 require "rbtv_sendeplan_bot/version"
 require "rbtv_sendeplan_bot/sendeplan"
 
+require 'reddit_bot'
+require 'optparse'
+
 WOCHENTAG = [:Sonntag, :Montag, :Dienstag, :Mittwoch, :Donnerstag, :Freitag, :Samstag, :Sonntag]
 MONAT = [nil,
          :Januar, :Februar, :März, :April, :Mai, :Juni,
          :Juli, :August, :September, :Oktober, :November, :Dezember]
 
+subreddit = nil
+
+OptionParser.new do |parser|
+  parser.separator "Show the RBTV programme schedule"
+  parser.separator ""
+  parser.separator "Options:"
+  parser.on('-r', '--subreddit=SUBREDDIT', 'Comma separated list of subreddits') {|r| subreddit = "r/#{r}" }
+end.parse!
+
 sendeplan = RbtvSendeplanBot::Sendeplan.new
 w = sendeplan.weekly_schedule
 
-text = ""
+text = []
 
 [Date.today, Date.today+1].each {|day|
-  text << "Programm vom #{WOCHENTAG[day.wday]}, dem #{day.day}. #{MONAT[day.month]} #{day.year}\n\n"
+  titel = ""
+  titel << "**" if subreddit
+  titel << "Programm vom #{WOCHENTAG[day.wday]}, dem #{day.day}. #{MONAT[day.month]} #{day.year}"
+  titel << "**" if subreddit
+  text << titel
 
   w.select {|e| e[:day] == day }.each {|e|
     if [:live, :premiere].include? e[:type]
-      text << "#{e[:starttime]} [#{e[:type].to_s[0].upcase}] #{e[:title]} (#{e[:duration]/60} Minuten)\n"
+      programme = ""
+      programme << "#{e[:starttime]} [#{e[:type].to_s[0].upcase}] "
+      programme << "*" if subreddit && e[:type] == :live
+      programme << "#{e[:title]} (#{e[:duration]/60} Minuten)"
+      programme << "*" if subreddit && e[:type] == :live
+      text << programme
     end
   }
-  text << "\n"
+  text << ""
 }
-text << "Der vollständige Sendeplan von RBTV ist unter https://rocketbeans.tv/sendeplan zu finden.\n"
-puts text
+text << "Dieses Posting wird täglich aktualisiert. Der vollständige Sendeplan von RBTV ist unter https://rocketbeans.tv/sendeplan zu finden."
+
+if subreddit
+  today = Date.today
+  title = "Sendeplan-Thread der Kalenderwoche #{today.strftime('%-V')} des Jahres #{today.year}"
+  bot = RedditBot::Bot.new YAML.load(File.read "secrets.yaml")
+  bot.json :post, "/api/submit", {
+    sr: subreddit,
+    kind: "self",
+    title: title,
+    text: text.join("\n\n"),
+  }
+else
+  puts text
+end
