becf75f0650afe36f2360444322f5208c61a2d00
Patric Mueller <bhaak@gmx.net>
2020-07-27 18:41:58 +0200

    Stdout output of today's and tomorrow's RBTV schedule

--- lib/rbtv_sendeplan_bot.rb
+++ lib/rbtv_sendeplan_bot.rb
@@ -1,8 +1,25 @@
 require "rbtv_sendeplan_bot/version"
+require "rbtv_sendeplan_bot/sendeplan"
 
-module RbtvSendeplanBot
-  class Error < StandardError; end
-  # Your code goes here...
-end
+WOCHENTAG = [:Sonntag, :Montag, :Dienstag, :Mittwoch, :Donnerstag, :Freitag, :Samstag, :Sonntag]
+MONAT = [nil,
+         :Januar, :Februar, :März, :April, :Mai, :Juni,
+         :Juli, :August, :September, :Oktober, :November, :Dezember]
 
-puts 'nothing to see here'
+sendeplan = RbtvSendeplanBot::Sendeplan.new
+w = sendeplan.weekly_schedule
+
+text = ""
+
+[Date.today, Date.today+1].each {|day|
+  text << "Programm vom #{WOCHENTAG[day.wday]}, dem #{day.day}. #{MONAT[day.month]} #{day.year}\n\n"
+
+  w.select {|e| e[:day] == day }.each {|e|
+    if [:live, :premiere].include? e[:type]
+      text << "#{e[:starttime]} [#{e[:type].to_s[0].upcase}] #{e[:title]} (#{e[:duration]/60} Minuten)\n"
+    end
+  }
+  text << "\n"
+}
+text << "Der vollständige Sendeplan von RBTV ist unter https://rocketbeans.tv/sendeplan zu finden.\n"
+puts text
---
+++ lib/rbtv_sendeplan_bot/sendeplan.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'json'
+
+module RbtvSendeplanBot
+  class Sendeplan
+    def initialize weekday=Date.today
+      # ensure it's a Monday
+      weekday = weekday - weekday.cwday + 1
+
+      startDay = weekday.to_time + 10*60*60
+      endDay   = startDay + 7*86400 - 1
+      url = "https://api.rocketbeans.tv/v1/schedule/normalized?startDay=#{startDay.to_i}&endDay=#{endDay.to_i}"
+      URI.open(url) {|request|
+        @schedule = JSON.parse(request.read, symbolize_names: true)
+      }
+    end
+
+    def weekly_schedule weekday=Date.today
+      @schedule[:data].map {|day|
+        #puts Time.parse(day[:date]).localtime.to_date
+        day[:elements].map {|entry|
+          sub_title = entry[:topic].to_s.strip
+          title = entry[:title].to_s.strip
+          title = sub_title.empty? ? title : "#{title} - #{sub_title}"
+          start_time = Time.parse(entry[:timeStart]).localtime.strftime("%H:%M")
+
+          {
+            day: Time.parse(day[:date]).localtime.to_date,
+            starttime: start_time,
+            title: title,
+            duration: entry[:duration],
+            type: entry[:type].to_sym,
+          }
+        }
+      }.flatten
+    end
+
+  end
+end
